a22f22e77960fe07540b381b3e3d6ac0e267f690
Sam Human <sam.huaman@gmail.com>
2019-04-26 23:54:07 -0500

    Last commit

--- src/app/listar-cliente/listar-cliente.component.ts
+++ src/app/listar-cliente/listar-cliente.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ClienteService} from '../services/cliente.service';
 import {Router} from '@angular/router';
 
+import * as _moment from 'moment';
+
 @Component({
   selector: 'app-listar-cliente',
   templateUrl: './listar-cliente.component.html',
@@ -10,21 +12,57 @@ import {Router} from '@angular/router';
 export class ListarClienteComponent implements OnInit {
 
   dataSource: Array<any>;
-  displayedColumns: string[] = ['Nombres', 'Apellidos', 'Edad', 'FechaNacimiento'];
+  displayedColumns: string[] = ['Nombres', 'Apellidos', 'Edad', 'FechaNacimiento', 'FechaDeceso'];
+
+  avg: number;
+  standardDeviation: number;
 
   constructor(private clientes: ClienteService,
               private router: Router) {
   }
 
   ngOnInit() {
+    this.avg = 0;
+    this.standardDeviation = 0;
     this.getClientes();
   }
 
   getClientes() {
     this.clientes.getClientes().subscribe((res) => {
-      this.dataSource = res.map((item) => item.payload.doc.data());
-      console.log(this.dataSource);
+      this.dataSource = res.map((item) => {
+        return {
+          Nombre: item.payload.doc.data().Nombre,
+          Apellido: item.payload.doc.data().Apellido,
+          Edad: item.payload.doc.data().Edad,
+          FechaNacimiento: item.payload.doc.data().FechaNacimiento,
+          Deceso: _moment(item.payload.doc.data().FechaNacimiento, 'D/M/YYYY')
+            .add(80, 'years').format('D/M/YYYY')
+        };
+      });
+      if (this.dataSource && this.dataSource.length) {
+        this.avg = this.getAverage(this.dataSource.map((cliente) => cliente.Edad));
+        this.getStandarDeviation();
+        console.log(this.avg);
+        console.log(this.standardDeviation);
+      }
     });
+  }
+
+  private getAverage(data: Array<any>): number {
+    const sum = data.reduce((value, init) => {
+      return Number(init) + Number(value);
+    });
+    return sum / this.dataSource.length;
+  }
+
+  private getStandarDeviation() {
+    const squareDiffs = this.dataSource.map((cliente) => {
+      const diff = cliente.Edad - this.avg;
+      return diff * diff;
+    });
+
+    const avgSquareDiff = this.getAverage(squareDiffs);
+    this.standardDeviation = Number(Math.sqrt(avgSquareDiff).toFixed(3));
   }
 
   redirectToRegister() {
